b850572aea8bcb3e5078769a8dbda17b0eb8fdba
guillermosifu <sifuentes1201@gmail.com>
2022-11-25 22:57:14 -0500

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,168 @@
-import { Fragment, useState } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-
-import './App.css'
+import React, { Fragment, useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Product from './components/Product';
+import Cart from './components/Cart';
+import './App.css';
 
 function App() {
 
-  const [productos] =useState([
+  const [products] = useState([
     {
       id: 1,
-      name: 'Polo de react',
-      mainCategory:'Tecnology',
-      secondaryCategory:'React',
+      name: 'Polo de React 2',
+      mainCategory: 'Technology',
+      secondaryCategory: 'React',
       price: 50,
-      image:"https://res.cloudinary.com/practicaldev/image/fetch/s--CSfu32Lv--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/jh26ac7tpt5o1i24dm6n.png"
-
+      image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--CSfu32Lv--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/jh26ac7tpt5o1i24dm6n.png'
     },
-    { id: 2,
-      name: 'Polo de react',
-      mainCategory:'Tecnology',
-      secondaryCategory:'React',
+    {
+      id: 2,
+      name: 'Polo de React 2',
+      mainCategory: 'Technology',
+      secondaryCategory: 'React',
       price: 50,
-      image:"https://res.cloudinary.com/practicaldev/image/fetch/s--CSfu32Lv--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/jh26ac7tpt5o1i24dm6n.png"
-
+      image: 'https://wallpaperaccess.com/full/3949076.jpg'
     },
-    ,
-    { id: 3,
-      name: 'Polo de react',
-      mainCategory:'Tecnology',
-      secondaryCategory:'React',
+    {
+      id: 3,
+      name: 'Polo de React 3',
+      mainCategory: 'Technology',
+      secondaryCategory: 'React',
       price: 50,
-      image:"https://res.cloudinary.com/practicaldev/image/fetch/s--CSfu32Lv--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/jh26ac7tpt5o1i24dm6n.png"
-
+      image: 'https://blog.facialix.com/wp-content/uploads/2021/03/react-redux.jpg'
     },
-    ,
-    { id: 4,
-      name: 'Polo de react',
-      mainCategory:'Tecnology',
-      secondaryCategory:'React',
+    {
+      id: 4,
+      name: 'Polo de Angular 1',
+      mainCategory: 'Technology',
+      secondaryCategory: 'Angular',
       price: 50,
-      image:""
-
+      image: 'https://miro.medium.com/max/1838/1*5eGMBtSNu9GVU8pNJC2akQ.png'
     },
-    ,
-    { id: 5,
-      name: 'Polo de react',
-      mainCategory:'Tecnology',
-      secondaryCategory:'React',
+    {
+      id: 5,
+      name: 'Polo de Angular 2',
+      mainCategory: 'Technology',
+      secondaryCategory: 'Angular',
       price: 50,
-      image:"https://res.cloudinary.com/practicaldev/image/fetch/s--CSfu32Lv--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/jh26ac7tpt5o1i24dm6n.png"
-
+      image: 'https://miro.medium.com/max/1838/1*5eGMBtSNu9GVU8pNJC2akQ.png'
     },
-    
+    {
+      id: 6,
+      name: 'Polo de Angular 3',
+      mainCategory: 'Technology',
+      secondaryCategory: 'Angular',
+      price: 50,
+      image: 'https://miro.medium.com/max/1838/1*5eGMBtSNu9GVU8pNJC2akQ.png'
+    },
+    {
+      id: 7,
+      name: 'Polo de JavaScript 1',
+      mainCategory: 'Technology',
+      secondaryCategory: 'JavaScript',
+      price: 40,
+      image: 'https://cutewallpaper.org/21/javascript-wallpaper/JavaScript-Wallpapers-Wallpaper-Cave.jpg'
+    },
+    {
+      id: 8,
+      name: 'Polo de JavaScript 2',
+      mainCategory: 'Technology',
+      secondaryCategory: 'JavaScript',
+      price: 40,
+      image: 'https://midu.dev/images/wallpapers/javascript-small-horizontal-4k.png'
+    },
+    {
+      id: 9,
+      name: 'Polo de JavaScript 3',
+      mainCategory: 'Technology',
+      secondaryCategory: 'JavaScript',
+      price: 40,
+      image: 'https://midu.dev/images/wallpapers/web-technologies-4k-wallpaper.png'
+    },
+    {
+      id: 10,
+      name: 'Polo de JavaScript 4',
+      mainCategory: 'Technology',
+      secondaryCategory: 'JavaScript',
+      price: 40,
+      image: 'https://wallpaperbat.com/img/364232-graphics.png'
+    },
+    {
+      id: 11,
+      name: 'Polo de JavaScript 4',
+      mainCategory: 'Technology',
+      secondaryCategory: 'JavaScript',
+      price: 40,
+      image: 'https://wallpaperbat.com/img/364232-graphics.png'
+    },
+    {
+      id: 12,
+      name: 'Polo de JavaScript 4',
+      mainCategory: 'Technology',
+      secondaryCategory: 'JavaScript',
+      price: 40,
+      image: 'https://wallpaperbat.com/img/364232-graphics.png'
+    }
+  ]);
 
-    
-  ])
+  const [cart, addProduct] = useState([]);
 
-  const currentDate = new Date().getFullYear()
-
+  const currentDate = new Date().getFullYear();
 
   return (
     <Fragment>
       <Header
-      titulito="tienda De Ropa"
-      store="el loco venta"
-      slogan="si vienes compras o mueres.."/>
-     
-  <Footer
-  currentDate={currentDate}
-  />
+        title='Tienda de Ropa Personalizada'
+        store='El loco venta'
+        slogan='si no compra mueres'
+      />
+
+      <main>
+        <section className="container">
+          <div className="row align-items-start">
+
+            <section className="col-md-9 my-2">
+              <div className="row align-items-start animate__animated animate__fadeInUp">
+                <div className="container__title">
+                  <h2>Productos</h2>
+                </div>
+                {products.map((product) => {
+                  return (
+                    <Product
+                      key={product.id}
+                      product={product}
+                      products={products}
+                      cart={cart}
+                      addProduct={addProduct}
+                    />
+                  );
+                })}
+              </div>
+            </section>
+
+            <section className="col-md-3 my-2">
+              <div className="row align-items-start animate__animated animate__fadeInUp">
+                <div className="container__title">
+                  <h2>Carrito</h2>
+                </div>
+                <Cart
+                  cart={cart}
+                  addProduct={addProduct}
+                />
+              </div>
+            </section>
+
+          </div>
+        </section>
+      </main>
+
+      <Footer
+        currentDate={currentDate}
+      />
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,42 +1,120 @@
+import React, { Fragment } from 'react';
+import Swal from 'sweetalert2'
 
-const{id,name,mainCategory,secondaryCategory,price,image}=productos;
+const Product = ({ product, cart, addProduct, products }) => {
 
-const  addProductCart=(id) =>{
-    const productos={
-        id:id,
-        name: name,
-        mainCategory : mainCategory,
-        secondaryCategory: secondaryCategory,
-        price:price,
-        image:image
+  const { id, name, mainCategory, secondaryCategory, price, image } = product;
+
+  const addProductCart = (id) => {
+    const product = {
+      id: id,
+      name: name,
+      mainCategory: mainCategory,
+      secondaryCategory: secondaryCategory,
+      price: price,
+      image: image
     };
-    const existProduct= cart.filter((productos)=>{
-        return productos.id === id;
+
+    const existProduct = cart.filter((product) => {
+      return product.id === id;
     });
 
-    if (existProduct[0]){
-        Swal.fire({
-            position:"center",
-            title: "el producto ya fue seleccionado",
-            text:"Elije otro producto",
-            icon: "warning",
-            confirmButtonText: "aceptar",
-            timer:4000
-        });
-    }else{
-        addProduct([
-            ...cart,
-            product
-        ])
+    if (existProduct[0]) {
+      Swal.fire({
+        position: 'center',
+        title: 'El producto ya fue seleccionado.',
+        text: 'Elegir otro producto por favor.',
+        icon: 'warning',
+        confirmButtonText: 'Aceptar',
+        timer: 4000
+      });
+    } else {
+      addProduct([
+        ...cart,
+        product
+      ]);
     }
+  };
+
+  const deleteProduct = () => {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: '¿Estás segur@?',
+      text: "¡No podrás revertir esto!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: '¡Sí, bórralo!',
+      cancelButtonText: '¡No, cancélalo!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const products = cart.filter((product) => {
+          return product.id !== id;
+        });
+        addProduct(products);
+        swalWithBootstrapButtons.fire(
+          '¡Eliminado!',
+          'Ha sido eliminado.',
+          'success'
+        );
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        swalWithBootstrapButtons.fire(
+          'Cancelad@',
+          'Está seguro :)',
+          'error'
+        );
+      }
+    });
+  };
+
+  return (
+    <Fragment>
+      {products ?
+        <section className="col-md-4 my-2">
+          <div className="card text-center bg-dark text-light" >
+            <a href={image} target="_blank" rel="noreferrer" className="over-flow-h">
+              <img src={image} className="card-img-top" alt={name} />
+            </a>
+            <div className="card-body">
+              <h4 className="card-title">{name}</h4>
+              <h5 className="card-subtitle my-2">Main: {mainCategory}</h5>
+              <h6 className="card-subtitle text-muted mb-2">{secondaryCategory}</h6>
+              <p className="card-text">S/ {price}</p>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => addProductCart(id)}
+              >Agregar</button>
+            </div>
+          </div >
+        </section >
+        :
+        <section className="col-md-12 my-2">
+          <div className="card text-center bg-dark text-light animate__animated animate__fadeInUp" >
+            <a href={image} target="_blank">
+              <img src={image} className="card-img-top" alt={name} />
+            </a>
+            <div className="card-body">
+              <h4 className="card-title">{name}</h4>
+              <h5 className="card-subtitle my-2">Main: {mainCategory}</h5>
+              <h6 className="card-subtitle text-muted mb-2">{secondaryCategory}</h6>
+              <p className="card-text">S/ {price}</p>
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={() => deleteProduct(id)}
+              >Eliminar</button>
+            </div>
+          </div >
+        </section >}
+    </Fragment>
+  );
 }
 
-const deleteProduct =()=>{
-    const swalWithBoostrapButtons = Swal.mixin({
-        customClass :{
-            confirmButton : 'btn btn-success',
-            cancelButton : 'btn btn-danger'
-        },
-        buttonStyling:false
-    })
-}
+export default Product;
